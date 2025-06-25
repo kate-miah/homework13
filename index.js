4f314a0a3c70241e3c712d56c43c35652519c910
kate-miah <ekaterin89@gmail.com>
2025-06-24 23:59:32 -0500

    done

---
+++ index.js
@@ -0,0 +1,50 @@
+//Завдання на змінні:
+
+let a = 4;
+let b = 5;
+let result1 = a * b;
+console.log(result1);
+
+let c = 30;
+let d = 6;
+let result2 = c / d;
+console.log(result2);
+
+let e = 7;
+let f = 4;
+let result3 = e + f;
+console.log(result3);
+
+console.log(typeof(11));
+console.log(typeof(true));
+console.log(typeof('javas script'));
+console.log(typeof('100'));
+
+let num  = 1;
+num = num += 11;
+num = num -= 11;
+num = num *= 11;
+num = num /= 11;
+num = num ++;
+num = num --;
+console.log(num);
+
+//Завдання на введення/виведення:
+
+// let number = prompt('What is your number?');
+// let userNumber = number**2;
+// alert('Your number is ' + userNumber);
+
+// let number1st = prompt('What is your 1st number?');
+// let number2nd = prompt('What is your 2nd number?');
+// let sum = (Number(number1st) + Number(number2nd)) / 2;
+// alert('Your number is ' + sum);
+
+// let minutes = prompt('Enter the number of minutes:')
+// let seconds = minutes * 60;
+// alert(minutes + ' minutes is ' +seconds + ' seconds')
+
+let greeting = 'Hello,';
+let userName = prompt('What is your name?');
+alert(greeting + ' ' + userName + '!');
+
